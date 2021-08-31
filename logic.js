293621f6f1a0909d1481562c308c75a6e53c677c
Scott Wells <45832584+scott-wells@users.noreply.github.com>
2021-08-31 10:27:13 -0700

    added branching logic for third question; bug discovered

--- logic.js
+++ logic.js
@@ -1,118 +1,127 @@
 const makeObjectsClickable = () => {
-    const objects = document.querySelectorAll(".object");
-    const addMessage = document.querySelector(".message-div");
-    const object1 = document.querySelector("#o1");
-    const object2 = document.querySelector("#o2");
-    const object3 = document.querySelector("#o3");
-  
-    const questions = {
-      q1: `I spy something that rhymes with <em>BUG</em>. Can you find it?`,
-      q2: `I spy something that rhymes with <em>WILLOW</em>. Can you spot anything?`,
-      q3: `I spy something that starts with an <em>LLL</em> sound. Can you find one?`
-    };
-    const responses = {
-      res1: `Yes, <em>RUG</em> rhymes with <em>BUG</em>!`,
-      res2: `Yes, <em>PILLOW</em> rhymes with <em>WILLOW</em>!`,
-      res3: `Yes, lamp starts with an <em>LLL</em> sound!`,
-      res4: `Try again.`
-    };
-  
-    // Load Question 1.
-    const loadQuestion1 = () => {
-      const question1 = questions.q1;
-      const p = document.createElement("P");
-      p.classList.add("message", "animate-text");
-      p.innerHTML = `${question1}`;
-      addMessage.appendChild(p);
-  
-      object1.addEventListener("click", (e) => {
-        console.log("You clicked Object 1");
-        const message = document.querySelector(".message");
-        message.remove();
-        const p = document.createElement("P");
-        p.classList.add("message", "animate-text");
-        p.innerHTML = `${responses.res1}`;
-        addMessage.appendChild(p);
-        setTimeout(loadQuestion2, 5000);
-      });
-  
-      object2.addEventListener("click", (e) => {
-        console.log("You clicked the wrong object!");
-        const message = document.querySelector(".message");
-        message.remove();
-        const p = document.createElement("P");
-        p.classList.add("message", "animate-text");
-        p.innerHTML = `${responses.res4}`;
-        addMessage.appendChild(p);
-        setTimeout(function () {
-          const message = document.querySelector(".message");
-          message.remove();
-          loadQuestion1();
-        }, 2000);
-      });
-  
-      object3.addEventListener("click", (e) => {
-        console.log("You clicked the wrong object!");
-        const message = document.querySelector(".message");
-        message.remove();
-        const p = document.createElement("P");
-        p.classList.add("message", "animate-text");
-        p.innerHTML = `${responses.res4}`;
-        addMessage.appendChild(p);
-        setTimeout(function () {
-          const message = document.querySelector(".message");
-          message.remove();
-          loadQuestion1();
-        }, 2000);
-      });
-    };
-  
-    // Question 2
-    const loadQuestion2 = () => {
-      const message = document.querySelector(".message");
-      message.remove();
-      const question2 = questions.q2;
-      const p = document.createElement("P");
-      p.classList.add("message", "animate-text");
-      p.innerHTML = `${question2}`;
-      addMessage.appendChild(p);
-  
-      object1.addEventListener("click", (e) => {
-        console.log("You clicked the wrong object!");
-        const message = document.querySelector(".message");
-        message.remove();
-        const p = document.createElement("P");
-        p.classList.add("message", "animate-text");
-        p.innerHTML = `${responses.res4}`;
-        addMessage.appendChild(p);
-        setTimeout(loadQuestion2, 2000);
-      });
-  
-      object2.addEventListener("click", (e) => {
-        console.log("You clicked Object 2!");
-        const message = document.querySelector(".message");
-        message.remove();
-        const p = document.createElement("P");
-        p.classList.add("message", "animate-text");
-        p.innerHTML = `${responses.res2}`;
-        addMessage.appendChild(p);
-        setTimeout(loadQuestion3, 5000);
-      });
-  
-      object3.addEventListener("click", (e) => {
-        console.log("You clicked the wrong object!");
-        const message = document.querySelector(".message");
-        message.remove();
-        const p = document.createElement("P");
-        p.classList.add("message", "animate-text");
-        p.innerHTML = `${responses.res4}`;
-        addMessage.appendChild(p);
-        setTimeout(loadQuestion2, 2000);
-      });
-    };
-  
-    loadQuestion1();
+  const messageDiv = document.querySelector(".message-div");
+  const object1 = document.querySelector("#o1");
+  const object2 = document.querySelector("#o2");
+  const object3 = document.querySelector("#o3");
+
+  const questions = {
+    q1: `I spy something that rhymes with <em><strong>bug</strong></em>. Can you find it?`,
+    q2: `I spy something that rhymes with <em><strong>willow</strong></em>. Can you spot anything?`,
+    q3: `I spy something that starts with an <em><strong>lll</strong></em> sound. Can you find one?`
   };
-  
-  makeObjectsClickable();
-  
+  const responses = {
+    res1: `Yes, <em><strong>rug</strong></em> rhymes with <em><strong>bug</strong></em>!`,
+    res2: `Yes, <em><strong>pillow</strong></em> rhymes with <em><strong>willow</strong></em>!`,
+    res3: `Yes, lamp starts with an <em><strong>lll</strong></em> sound!`,
+    res4: `Try again.`
+  };
+
+  const addMessage = (message) => {
+    const p = document.createElement("P");
+    p.classList.add("message", "animate-text");
+    p.innerHTML = `${message}`;
+    messageDiv.appendChild(p);
+  };
+
+  const selectRemoveMessage = () => {
+    const message = document.querySelector(".message");
+    message.remove();
+  };
+
+  // Question 1
+  const loadQuestion1 = () => {
+    addMessage(questions.q1);
+
+    object1.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res1);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion2();
+      }, 5000); // turn this into a button instead
+    });
+
+    object2.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res4);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion1();
+      }, 3000);
+    });
+
+    object3.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res4);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion1();
+      }, 3000);
+    });
+  };
+
+  // Question 2
+  const loadQuestion2 = () => {
+    addMessage(questions.q2);
+
+    object1.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res4);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion2();
+      }, 3000);
+    });
+
+    object2.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res2);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion3();
+      }, 5000); // turn this into a button instead
+    });
+
+    object3.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res4);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion2();
+      }, 3000);
+    });
+  };
+
+  // Question 3
+  const loadQuestion3 = () => {
+    addMessage(questions.q3);
+
+    object1.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res4);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion3();
+      }, 3000);
+    });
+
+    object2.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res4);
+      setTimeout(() => {
+        selectRemoveMessage();
+        loadQuestion3();
+      }, 3000);
+    });
+
+    object3.addEventListener("click", () => {
+      selectRemoveMessage();
+      addMessage(responses.res3);
+      // break;
+    });
+  };
+
+  loadQuestion1();
+};
+
+makeObjectsClickable();
